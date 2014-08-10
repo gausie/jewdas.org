76534664ae5034d81c0b0ae4f0cac1874e89e85b
Sam Gaus <git@gaus.ie>
2014-08-10 13:58:28 +0100

    Include navigation bar in footer nav

--- lib/extras.php
+++ lib/extras.php
@@ -20,3 +20,22 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+function add_search_to_wp_menu($items, $args) {
+  if($args->theme_location === 'footer_navigation') {
+    $items .= '<li class="menu-search">';
+    $items .= load_template_part('templates/searchform');
+    $items .= '</li>';
+  }
+
+  return $items;
+}
+add_filter('wp_nav_menu_items','add_search_to_wp_menu',10,2);
+
+function load_template_part($template_name, $part_name=null) {
+    ob_start();
+    get_template_part($template_name, $part_name);
+    $var = ob_get_contents();
+    ob_end_clean();
+    return $var;
+}
